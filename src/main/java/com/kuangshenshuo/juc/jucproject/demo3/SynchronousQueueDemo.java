25a8b2b4fc6351a83c46b497782a614e0b3ec660
SYWUkenan <251311992@qq.com>
2021-03-05 23:24:47 +0800

    三个辅助类 countDownLatch CylicBarrier semaphore

---
+++ src/main/java/com/kuangshenshuo/juc/jucproject/demo3/SynchronousQueueDemo.java
@@ -0,0 +1,52 @@
+package com.kuangshenshuo.juc.jucproject.demo3;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 同步队列
+ * @author yswu
+ * @date 2021-03-04 10:58
+ */
+public class SynchronousQueueDemo {
+    public static void main(String[] args) {
+        BlockingQueue<String> synchronousQueue = new SynchronousQueue<>();
+        new Thread(()->{
+
+            try {
+                System.out.println(Thread.currentThread().getName()+"put 1");
+                synchronousQueue.put("1");
+                System.out.println(Thread.currentThread().getName()+"put 2");
+                synchronousQueue.put("2");
+                System.out.println(Thread.currentThread().getName()+"put 3");
+                synchronousQueue.put("3");
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        },"T1").start();
+
+        new Thread(()->{
+            try {
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println(Thread.currentThread().getName()+" "+synchronousQueue.take());
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println(Thread.currentThread().getName()+" "+synchronousQueue.take());
+                TimeUnit.SECONDS.sleep(3);
+                System.out.println(Thread.currentThread().getName()+" "+synchronousQueue.take());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        },"T2").start();
+
+
+
+    }
+
+
+}
+
+
