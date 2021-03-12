281046759b25f585d558c8b9aa0cf28257098c2f
SYWUkenan <251311992@qq.com>
2021-03-13 00:17:03 +0800

    快捷键测试

--- src/main/java/com/kuangshenshuo/juc/jucproject/demo1/test1.java
+++ src/main/java/com/kuangshenshuo/juc/jucproject/demo1/test1.java
@@ -1,5 +1,7 @@
 package com.kuangshenshuo.juc.jucproject.demo1;
 
+import java.util.ArrayList;
+
 /**
  * @author yswu
  * @date 2021-02-24 10:45
@@ -7,7 +9,23 @@ package com.kuangshenshuo.juc.jucproject.demo1;
 
 public class test1 {
     public static void main(String[] args) {
-        //获取cpu的核数
-        System.out.println(Runtime.getRuntime().availableProcessors());
+        ArrayList<String > list = new ArrayList<>();
+
+//        new ArrayList<>();
+        list.add("asfd");
+        list.add("asdd");
+        list.add("assdd");
+
+        for (String s : list) {
+            System.out.println("s = " + s);
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+
+
+        }
     }
+
+
+
 }
--- src/test/java/com/kuangshenshuo/juc/jucproject/JucprojectApplicationTests.java
+++ src/test/java/com/kuangshenshuo/juc/jucproject/JucprojectApplicationTests.java
@@ -10,4 +10,32 @@ class JucprojectApplicationTests {
     void contextLoads() {
     }
 
+    public static void main(String[] args) {
+
+
+        String name = "wuyusheng";
+
+        //从10输出  10.for
+        for (int j = 0; j < 10; j++) {
+
+        }
+
+        for (int m = 0; m < 10; m++) {
+
+        }
+
+        String[] names = {"a","b","c"};
+
+        for (String s : names) {
+            System.out.println(s);
+        }
+
+        String a = getName();
+        System.out.println("Hello world");
+    }
+
+    public static String getName(){
+        return "wu";
+    }
+
 }
